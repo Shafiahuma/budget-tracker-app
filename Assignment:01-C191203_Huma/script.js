0dd5605d823335c0d60e823164402422a1530982
Shafia Nur Huma <50522609+Shafiahuma@users.noreply.github.com>
2023-12-20 15:05:58 +0600

    Assignment 01

--- Assignment:01-C191203_Huma/script.js
+++ Assignment:01-C191203_Huma/script.js
@@ -7,6 +7,8 @@ const selectInput = document.getElementById("type");
 const incomeList = document.getElementById("income-list");
 const expenseList = document.getElementById("expense-list");
 const totalIncome = document.getElementById("total-income");
+const totalExpense = document.getElementById("total-expense");
+const totalBudget = document.getElementById("total-budget");
 
 function formatMoney(value) {
   return Math.abs(Number(value)).toLocaleString(undefined, {
@@ -30,18 +32,80 @@ calculateIncome();
 /**
  * Task 1: Calculate total expense
  */
-function calculateExpense() {}
+function calculateExpense() {
+  // Initialize a variable 'sum' to keep track of the total expense
+  let sum = 0;
+
+  // Iterate over the children of the 'expenseList' element
+  for (let item of expenseList.children) {
+    const valueString =
+      item.children[0].children[1].children[0].innerHTML.replace(/,/g, "");
+
+    // Convert the extracted value string to a floating-point number and log it to the console
+    console.log(parseFloat(valueString));
+
+    // Add the converted value to the 'sum' variable
+    sum += parseFloat(valueString);
+  }
+
+  // Set the inner HTML of the 'totalExpense' element to the formatted total expense
+  totalExpense.innerHTML = formatMoney(sum);
+}
+
+calculateExpense()
 
 /**
  * Task 2: Calculate the budget
  */
 
-function calculateBudget() {}
+function calculateBudget() {
+  // Extract the total income value from the inner HTML of the 'totalIncome' element
+  const totalIncomeValue = parseFloat(totalIncome.innerHTML.replace(/,/g, ""));
+
+  // Extract the total expense value from the inner HTML of the 'totalExpense' element
+  const totalExpenseValue = parseFloat(totalExpense.innerHTML.replace(/,/g, ""));
+
+  // Check if total expenses exceed total income
+  if (totalExpenseValue > totalIncomeValue) {
+    // Display an alert if total expenses are greater than total income
+    alert("Warning: Total expenses exceed total income!");
+  }
+
+  // Calculate the budget by subtracting total expenses from total income
+  const budget = totalIncomeValue - totalExpenseValue;
+
+  // Set the inner HTML of the 'totalBudget' element to the formatted budget value
+  totalBudget.innerHTML = formatMoney(budget);
+}
+calculateBudget();
 
 /**
  * Task 3: Delete Entry
  */
-function deleteEntry() {}
+function deleteEntry(listId, totalId) {
+  const list = document.getElementById(listId);
+
+  list.addEventListener("click", function (event) {
+    const deleteButton = event.target.closest(".text-red-500");
+
+    if (deleteButton) {
+      const listItem = deleteButton.closest("li");
+      const isIncome = listItem.querySelector(".text-green-600") !== null;
+      const valueElement = listItem.querySelector(isIncome ? '.text-green-600' : '.text-red-600');
+
+      if (valueElement) {
+        const value = parseFloat(valueElement.innerHTML.replace(/,/g, ""));
+        const totalElement = document.getElementById(totalId);
+
+        totalElement.innerHTML = formatMoney(parseFloat(totalElement.innerHTML.replace(/,/g, "")) - Math.abs(value));
+
+        calculateBudget();
+        listItem.remove();
+      }
+    }
+  });
+}
+
 
 function addEntry() {
   const type = selectInput.value;
@@ -87,6 +151,14 @@ function addEntry() {
 
   // update total income value
   calculateIncome();
+
+  // update total expense value
+  calculateExpense();
+
+  //update total balance
+  calculateBudget();
 }
 
 addExpenseButton.addEventListener("click", addEntry);
+deleteEntry("income-list", "total-income");
+deleteEntry("expense-list", "total-expense");
