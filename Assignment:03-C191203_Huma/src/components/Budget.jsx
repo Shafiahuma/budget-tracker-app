00668c584ae78a9970d5055d48daf331764fa54c
Shafia Nur Huma <snhuma2199@gmail.com>
2024-01-04 13:29:18 +0600

    next

--- Assignment:03-C191203_Huma/src/components/Budget.jsx
+++ Assignment:03-C191203_Huma/src/components/Budget.jsx
@@ -1,18 +1,30 @@
 //./node_modules/.bin/vite
 
 import { useEntries } from "../hooks/useEntries";
-
+import { useState, useEffect } from "react";
+import axios from 'axios';
 export default function Budget() {
-  const { totalIncome, totalExpense } = useEntries();
+  const { budget, setBudget } = useState('');
 
-  // Calculate the total budget
-  const totalBudget = totalIncome - totalExpense;
+  useEffect(() => {
+    const fetchbudgetData = async () => {
+      try{
+        const budgetResponse = await axios.get('http://localhost:3000/budget');
+
+        setBudget(budgetResponse.data);
+      } catch(error) {
+        console.error('Error fetching budget data:', error);
+      }
+    };
+
+    fetchbudgetData();
+  }, []);
 
   return (
     <div className="mx-auto max-w-sm px-5 py-8 text-center text-white">
       <div>
         <h2>Available Budget</h2>
-        <p className="mt-1 text-4xl font-medium">+ BDT {totalBudget.toFixed(2)}</p>
+        <p className="mt-1 text-4xl font-medium">+ BDT {budget}</p>
       </div>
 
       <div className="mt-4 flex items-center justify-between bg-green-500 px-4 py-3 text-sm">
